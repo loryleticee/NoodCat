12ba320bc6dd27700b3bc00db9cf60d5e087497b
YannouLR <yann.lenerand@yahoo.com>
2022-01-27 16:22:17 +0100

    router, Vues, route finish

---
+++ src/Controllers/CatBarControllers.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class CatBarControllers
+{
+    public function showAll()
+    {
+        
+    }
+
+    public function delete(string $sId)
+    {
+        
+    }
+    
+    public function modify(string $sId)
+    {
+        
+    }
+
+    public function add()
+    {
+        
+    }
+}
