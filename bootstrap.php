e4b86d7ebe17d6fa05d78d70213c5f5017ca32f2
NoodMan <camara.adeline@yahoo.fr>
2022-01-27 15:27:50 +0100

    correction bdd

--- bootstrap.php
+++ bootstrap.php
@@ -4,8 +4,11 @@ namespace App;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
+
 require_once "vendor/autoload.php";
 require_once 'dotenv.php';
+(new DotEnv())->load();
+
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
 $isDevMode = true;
@@ -17,7 +20,6 @@ $config->setAutoGenerateProxyClasses(1);
 // or if you prefer yaml or XML
 // $config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 // $config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
-
 // database configuration parameters
 $conn = array(
     'driver' => 'pdo_mysql',
